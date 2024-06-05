82039d5100d2cfc7564fb4ef507b6e1cb6bc83ce
10ankita <ankita.dubey10oct@gmail.com>
2024-06-05 14:51:37 +0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  const make = "Ford";
+  const model = "Mustang";
+  const car = { make, model };
+  console.log(car);
+  return (
+    <div className="">
+      <h1>Vrindavan Cafe</h1>
+    </div>
+  );
+}
+
+export default App;
